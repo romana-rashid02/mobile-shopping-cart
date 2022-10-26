636b3eb85f1a73fd819b21e99875188018a753e1
Romana Rashid <romana.rashid02@gmail.com>
2022-10-26 12:13:00 +0600

    case total price done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+    if(isIncrease == true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(true);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+})
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+})
